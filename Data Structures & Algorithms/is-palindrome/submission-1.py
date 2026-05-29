9406c46e3c3a61f5a2c9c4286e16d78c407240f4
Harshdeep Singh <166107684+Harsh-9th@users.noreply.github.com>
2026-05-29 20:23:48 +0530

    Add: is-palindrome - submission-1

---
+++ Data Structures & Algorithms/is-palindrome/submission-1.py
@@ -0,0 +1,27 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        chars = "abcdefghijklmnopqrstuvwxyz0123456789"
+        s = s.lower()
+
+        i, j = 0, len(s) - 1
+        while i < j:
+            if s[i] not in chars:
+                i += 1
+            
+            if s[j] not in chars:
+                j -= 1
+            
+            if s[i] in chars and s[j] in chars:
+                if s[i] != s[j]:
+                    return False
+                
+                i += 1
+                j -= 1
+        return True
+
+
+        
+
+
+
+        
